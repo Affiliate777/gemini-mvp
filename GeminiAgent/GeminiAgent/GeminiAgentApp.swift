0989607b30ed18c14edc48552d18534f5441713b
Affiliate777 <76167637+Affiliate777@users.noreply.github.com>
2025-12-01 17:46:55 +0530

    WIP: preserve current Finder badge work (moved off mvp-14 branch)

---
+++ GeminiAgent/GeminiAgent/GeminiAgentApp.swift
@@ -0,0 +1,17 @@
+//
+//  GeminiAgentApp.swift
+//  GeminiAgent
+//
+//  Created by Bret Barnard on 11/30/25.
+//
+
+import SwiftUI
+
+@main
+struct GeminiAgentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
